e1ad2f28e63f6c0f10a50fbbbcbba682a841a6a5
sabidao <x-sabidao@hotmail.com>
2017-12-15 15:48:52 -0200

    Accerto nome da ost

--- pasta web file finder/Game.cs
+++ pasta web file finder/Game.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net;
-using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
 namespace pasta_web_file_finder
@@ -11,7 +9,8 @@ namespace pasta_web_file_finder
     public class Game
     {
         private string url = String.Empty;
-        public string Nome { get; set; } = string.Empty;
+        private String nome = "";
+        public string Nome { get { return nome; } set { nome = WebUtility.HtmlDecode(value); } }
         public Dictionary<string, string> Tracks { get; set; } = new Dictionary<string, string>();
         public String OST_LIST_DUMP = (Application.StartupPath);
 
@@ -24,17 +23,18 @@ namespace pasta_web_file_finder
         public Game()
         {
 
-        }        
+        }
 
         public void LoadMusics(WebClient wc)
         {
-           
+
         }
 
         public void Download(ref WebClient wc, String track)
         {
             Directory.CreateDirectory(Path.Combine(Application.StartupPath, "OST", Nome));
-            wc.DownloadFileAsync(new Uri(track), Path.Combine(Application.StartupPath, "OST", Nome, track.Split('/')[track.Split('/').Length - 1].Replace('-', ' ')));
+            wc.Proxy = new WebProxy("192.168.113.245:3128") { UseDefaultCredentials = true, BypassProxyOnLocal = true };
+            wc.DownloadFileAsync(new Uri(track), Path.Combine(Application.StartupPath, "OST", Nome, WebUtility.UrlDecode(track.Split('/')[track.Split('/').Length - 1].Replace('-', ' '))));
         }
 
     }
